ab30e7fe5876fb97d3c5838f3fa3c0c41b0ced50
Wubpooz <mathieu.waharte@gmail.com>
2026-04-18 11:11:11 +0200

    remove tp1

--- tp1/src/main/java/games/dominos/DominosRole.java
+++
@@ -1,8 +0,0 @@
-package games.dominos;
-
-import iialib.games.model.IRole;
-
-public enum DominosRole implements IRole{ 
-		HORIZONTAL, 	// For the player playing its tiles hally
-		VERTICAL		// For the player playing its tiles vertically
-}
