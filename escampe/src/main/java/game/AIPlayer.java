2165e59f585f12ebe5451a4481d18da580c539ae
Wubpooz <mathieu.waharte@gmail.com>
2026-05-24 14:34:30 +0200

    merged Player into AIPlayer

--- escampe/src/main/java/game/AIPlayer.java
+++ escampe/src/main/java/game/AIPlayer.java
@@ -4,28 +4,44 @@ import interfaces.IBoard;
 import interfaces.IMove;
 import interfaces.IRole;
 
-public class AIPlayer<M extends IMove, R extends IRole, B extends IBoard<M,R,B>> extends Player<R> {
 
+public class AIPlayer<M extends IMove, R extends IRole, B extends IBoard<M,R,B>> {
+	public final R role;
 	private algorithms.GameAlgorithm<M,R,B> ai;
 
-	public void setAi(algorithms.GameAlgorithm<M, R, B> ai) {
-		this.ai = ai;
-	}
-
+  // Constructors
 	public AIPlayer(R role) {
-		super(role);
+		this.role = role;
 	}
 
 	public AIPlayer(R role, algorithms.GameAlgorithm<M,R,B> alg) {
-		super(role);
+    this(role);
 		this.ai = alg;
 	}
 
+  // Methods
 	public M bestMove(B board) {
 		return ai.bestMove(board,this.getRole());
 	}
 
 	public B playMove(B board, M move) {
 		return board.play(move, this.getRole());
+	}
+
+
+  // Setters and getters
+  public void setAi(algorithms.GameAlgorithm<M, R, B> ai) {
+		this.ai = ai;
+	}
+
+	public R getRole() {
+		return role;
+	}
+
+  @Override
+	public String toString() {
+		return "AIPlayer{" +
+        "role=" + role +
+        '}';
 	}
 }
